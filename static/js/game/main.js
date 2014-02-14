4a7edc329b4e2e18ef3d1bf4a0db53ed3b87bc43
Scott Adie <scottynosabe@gmail.com>
2014-02-14 18:53:53 -0500

    Simple fixes

--- static/js/game/main.js
+++ static/js/game/main.js
@@ -1,7 +1,9 @@
 var game = new Phaser.Game(800, 480, Phaser.AUTO, 'content', { preload: preload, create: create, update: update });
+
 var platforms;
 var player;
 var cursors;
+var fpsText;
 
 function preload() {
     game.load.image('sky', '/static/images/sky.png');
@@ -12,33 +14,33 @@ function preload() {
 function create() {
     game.add.sprite(0, 0, 'sky');
 
-     platforms = game.add.group();
+    platforms = game.add.group();
     for(var i = 0; i < (game.world.width/256)+1; i++) {
-        var ground = platforms.create(i*256, game.world.height-256, 'ground');
+        var ground = platforms.create(i*256, game.world.height-129, 'ground');
         ground.body.immovable = true;
     }
 
-    player = game.add.sprite(0, 0, 'char' );
-    //player.body.bounce.y = 0.2;
+    player = game.add.sprite(0, 0, 'char');
     player.body.gravity.y = 10;
-    //player.body.collideWorldBounds = true;
-    player.scale.setTo(.2, .75);
+    player.scale.setTo(.25, .25);
     player.body.velocity.y = 50;
+    player.body.velocity.x = 300;
 
     cursors = game.input.keyboard.createCursorKeys();
+    fpsText = game.add.text(16, 16, 'FPS: 0', { font: '16px arial', fill: '#000' });
 }
 
 function update() {
     game.physics.collide(player, platforms);
-    player.body.velocity.x = 300;
+    //player.body.velocity.x = 300;
 
-    if(cursors.up.isDown && player.body.touching.down)
-    {
+    if(cursors.up.isDown && player.body.velocity.y >= 0) {
         player.body.velocity.y = -200;
     }
     
-    if (Math.round(player.x + player.width) >= game.world.width)
-    {
+    if (Math.round(player.x + player.width) >= game.world.width) {
         player.x = 0;
     }
+    
+    fpsText.content = 'FPS: ' + game.time.fps;
 }
